361e0af5ba813a3fc20c101d86218c53b4b8e544
jf773 <jf773@cornell.edu>
2019-03-08 22:25:47 -0500

    abs

---
+++ sequential_column.c
@@ -0,0 +1,80 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+#include <limits.h>
+#include <sys/times.h>
+#include <sys/types.h>
+
+#define BILLION 1000000000L
+
+int main(int argc, char const *argv[])
+{
+    int m, n;
+    int i, j, k;
+    int n_threads;
+    int** mat;
+    int max_row;
+    int temp;
+    int nthreads;
+    int tid;
+    struct timespec start, end;
+    long long unsigned int diff;
+    
+
+    if (argc != 4) {
+        printf("missing argument: matrix row size, column size, number of thread");
+    }
+
+    m = atoi(argv[1]);
+    n = atoi(argv[2]);
+    n_threads = atoi(argv[3]);
+
+    // populate the matrix
+    mat = (int **)malloc(m * sizeof(int *)); 
+    for (i = 0; i < m; i++) {
+        mat[i] = (int*) malloc(n * sizeof(int));
+    }
+    srand(3);
+    for (i = 0; i < m; i++) {
+        for (j = 0; j < n; j++) {
+            mat[i][j] = rand();
+            printf("%d ", mat[i][j]);
+        }
+        printf("\n");
+    }
+
+    clock_gettime(CLOCK_MONOTONIC, &start);	/* mark start time */
+    // find the max abs element in each column and swap
+    for (i = 0; i < n; i++) {
+        int max = -1;
+        max_row = -1;
+        // find max abs element in each column
+       for (k = i; k < m; k++) {
+            if (abs(mat[k][i]) > max) {
+                max = abs(mat[k][i]);
+                max_row = k;
+            }
+        }
+
+        // print the pair
+        k = max_row;
+        if (k != -1) {
+            printf("the pair is: %d %d\n", i, k);
+        }
+
+        // swap row i and row k
+        if (i != k && k != -1) {
+            for (j = 0; j < n; j++) {
+                temp = mat[i][j];
+                mat[i][j] = mat[k][j];
+                mat[k][j] = temp;
+            }
+        }
+    }
+
+    clock_gettime(CLOCK_MONOTONIC, &end);	/* mark the end time */
+    diff = BILLION * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec;
+	printf("elapsed time = %llu milliseconds\n", (long long unsigned int) (diff / 1000000));
+    return 0;
+}
